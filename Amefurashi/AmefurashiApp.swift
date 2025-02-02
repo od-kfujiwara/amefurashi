09f13acb5f84c1e7de49c63426b933b9229d597b
koheylux <koheylux.0224f@pets-mail.com>
2025-02-02 16:45:03 +0900

    Initial Commit

---
+++ Amefurashi/AmefurashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmefurashiApp.swift
+//  Amefurashi
+//
+//  Created by 藤原光平 on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct AmefurashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amefurashi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Amefurashi
+//
+//  Created by 藤原光平 on 2025/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
